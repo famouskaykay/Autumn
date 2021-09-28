7d0b85f68b22ae777829a88fade6805d7007b0a1
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-09-28 23:26:40 +0000

    Create __init__.py

---
+++ autumn/__init__.py
@@ -0,0 +1,18 @@
+from pyrogram import Client, errors
+from autumn.config import Config
+import logging
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+
+APP_ID = Config.APP_ID
+API_HASH = Config.API_HASH
+TOKEN = Config.TOKEN
+BOT_ID = Config.BOT_ID
+OWNER_ID = Config.OWNER_ID
+
+cb = Client("Chatbot", api_id=APP_ID, api_hash=API_HASH, bot_token=TOKEN)
