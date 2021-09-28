15d7bba6c9f391bb269b2102d6c5975c50721236
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-09-28 23:39:59 +0000

    Create config.py

---
+++ autumn/config.py
@@ -0,0 +1,12 @@
+import os
+
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+API_ID = int(os.getenv("API_ID", "6"))
+API_HASH = os.getenv("API_HASH", "eb06d4abeb98ae0f581e")
+TOKEN = os.getenv("BOT_TOKEN")
+BOT_ID = os.getenv("BOT_ID")
+OWNER_ID = os.getenv("OWNER_ID")
+
