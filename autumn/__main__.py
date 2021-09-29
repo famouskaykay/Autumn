5e4af2dffda9e2dfa0cc6b23d4abd578de6615ab
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-09-29 03:45:19 +0000

    Update __main__.py

--- autumn/__main__.py
+++ autumn/__main__.py
@@ -1,6 +1,14 @@
 from autumn import kaykay
 from autumn.modules import start, chatbot
 from pyrogram import idle, Client
+import time
+
+MOD_LOAD = []
+MOD_NOLOAD = []
+bot_start_time = time.time()
+
+# for fun
+print("[INFO]: INITIALIZING ")
 
 kaykay.start()
 idle()
