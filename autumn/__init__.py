83e2bf56e7286b1ee77c025102e8573c09702bb0
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-09-29 09:22:46 +0000

    Update __init__.py

--- autumn/__init__.py
+++ autumn/__init__.py
@@ -2,6 +2,7 @@ from pyrogram import Client, errors
 from autumn.config import APP_ID, API_HASH, TOKEN, BOT_ID, OWNER_ID
 import logging
 from kaysconfig import LOG_GROUP_ID
+import time
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
@@ -10,5 +11,10 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
 LOG_GROUP_ID = LOG_GROUP_ID
+
+MOD_LOAD = []
+MOD_NOLOAD = []
+bot_start_time = time.time()
+
 
 kaykay = Client("Chatbot", api_id=APP_ID, api_hash=API_HASH, bot_token=TOKEN)
