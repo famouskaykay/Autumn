cd05d888dedc830871e5d2e31c61fb12fa09fb71
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-09-29 09:21:45 +0000

    Update __main__.py

--- autumn/__main__.py
+++ autumn/__main__.py
@@ -3,9 +3,6 @@ from autumn.modules import start, chatbot
 from pyrogram import idle, Client
 import time
 
-MOD_LOAD = []
-MOD_NOLOAD = []
-bot_start_time = time.time()
 
 # for fun
 print("[INFO]: INITIALIZING ")
