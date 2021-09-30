c10de7bd8bae199feeb9850697b6215ff8583845
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-09-30 18:46:05 +0000

    Update __init__.py

--- autumn/__init__.py
+++ autumn/__init__.py
@@ -1,8 +1,13 @@
+print("[INFO]: INITIALIZING")
 from pyrogram import Client, errors
 from autumn.config import APP_ID, API_HASH, TOKEN, BOT_ID, OWNER_ID
 import logging
+import asyncio
 from kaysconfig import LOG_GROUP_ID
 import time
+from os import path
+from aiogram import Bot, Dispatcher, types
+from pyromod import listen
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
@@ -14,7 +19,13 @@ LOG_GROUP_ID = LOG_GROUP_ID
 
 MOD_LOAD = []
 MOD_NOLOAD = []
-kaykay_start_time = time.time()
+bot_start_time = time.time()
 
 
-kaykay = Client("Chatbot", api_id=APP_ID, api_hash=API_HASH, bot_token=TOKEN)
+kaykay = Client("autumn", api_id=APP_ID, api_hash=API_HASH, bot_token=TOKEN)
+
+
+
+print("[INFO]: STARTING BOT CLIENT TEMPORARILY")
+kaykay.start()
+kaykay.stop()
