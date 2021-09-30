ef2fdf3eec6ae44b484c0d2bcc3c362c7b5f69bc
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-09-30 23:35:24 +0000

    Update __main__.py

--- autumn/__main__.py
+++ autumn/__main__.py
@@ -1,4 +1,5 @@
 import importlib
+from kaysconfig import LOG_GROUP_ID
 import asyncio
 import uvloop
 from autumn import kaykay
@@ -8,6 +9,39 @@ import time
 from autumn.modules import ALL_MODULES
 
 loop = asyncio.get_event_loop()
+
+
+async def start_bot():
+    bot_modules = ""
+    j = 1
+    for i in ALL_MODULES:
+        if j == 4:
+            bot_modules += "|{:<15}|\n".format(i)
+            j = 0
+        else:
+            bot_modules += "|{:<15}".format(i)
+        j += 1
+    print(
+        "+_________________________________________________________________"
+    )
+    print(
+        "|                           Autumn                               |"
+    )
+    print(
+        "+_________________________________________________________________+"
+    )
+    print(bot_modules)
+    print(
+        "+_________________________________________________________________+"
+    )
+    print(f"[INFO]: BOT STARTED !"))
+    try:
+        print("[INFO]: SENDING ONLINE STATUS")
+        await kaykay.send_message(LOG_GROUP_ID, "autumn has started!")
+    except Exception:
+        pass
+    await idle()
+    print("[INFO]: STOPPING BOT  ")   
 
 print("[INFO]: INITIALIZING ")
 
