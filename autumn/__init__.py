f8afdc446f654d100a3126facb4a885ed313db75
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-10-01 00:21:09 +0000

    Update __init__.py

--- autumn/__init__.py
+++ autumn/__init__.py
@@ -2,7 +2,6 @@ print("[INFO]: INITIALIZING")
 from pyrogram import Client, errors
 from autumn.config import APP_ID, API_HASH, TOKEN, BOT_ID, OWNER_ID
 import logging
-from telegraph import Telegraph
 import asyncio
 from kaysconfig import LOG_GROUP_ID
 import time
@@ -15,6 +14,7 @@ logging.basicConfig(
 )
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
+kaykay = Client("autumn", api_id=APP_ID, api_hash=API_HASH, bot_token=TOKEN)
 
 
 LOG_GROUP_ID = LOG_GROUP_ID
@@ -23,7 +23,6 @@ MOD_LOAD = []
 MOD_NOLOAD = []
 bot_start_time = time.time()
 
-kaykay = Client("autumn", api_id=APP_ID, api_hash=API_HASH, bot_token=TOKEN)
 
 print("[INFO]: STARTING BOT CLIENT ")
 kaykay.start()
