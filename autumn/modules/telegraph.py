af4e6280230d5c23c0f1904755ae5ab85be8be96
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-10-01 17:35:13 +0000

    Update telegraph.py

--- autumn/modules/telegraph.py
+++ autumn/modules/telegraph.py
@@ -8,43 +8,45 @@ from autumn.kay.decorators.errors import capture_err
 from autumn import telegraph
 
 
+@app.on_message(
+    filters.text
+    & filters.reply
+    & ~filters.bot
+    & ~filters.edited,
+    group=2,
+)
 
-
-
-
-
-
-
-@app.on_message(filters.photo)
+@app.on_message(filters.command("telegraph"))
 @capture_err
-async def telegraphphoto(client, message):
-    msg = await message.reply_text("Uploading To Telegraph...")
-    download_location = await client.download_media(
+async def paste(_, message: Message):
+    reply = message.reply_to_message
+
+    if not reply or not reply.text:
+        return await message.reply("Reply to a text message")
+
+    if len(message.command) < 2:
+        return await message.reply("**Usage:**\n /telegraph [Page name]")
+
+    page_name = message.text.split(None, 1)[1]
+    page = telegraph.create_page(page_name, html_content=reply.text.html)
+    return await message.reply(
+        f"**Posted:** {page['url']}",
+        disable_web_page_preview=True,
+    )
+    if len(message.photo):
+        msg = await message.reply_text("Uploading To Telegraph...")
+        download_location = await client.download_media(
         message=message, file_name='root/jetg')
-    try:
+        try:
         response = upload_file(download_location)
-    except:
+        except:
         await msg.edit_text("Photo size should be less than 5mb!") 
-    else:
+        else:
         await msg.edit_text(f'**Uploaded To Telegraph!\n\n👉 https://telegra.ph{response[0]}\n\nJoin **',
             disable_web_page_preview=True,
         )
-    finally:
+        finally:
         os.remove(download_location)
 
-@app.on_message(filters.video)
-@capture_err
-async def telegraphvid(client, message):
-    msg = await message.reply_text("Uploading To Telegraph...")
-    download_location = await client.download_media(
-        message=message, file_name='root/jetg')
-    try:
-        response = upload_file(download_location)
-    except:
-        await msg.edit_text("Video size should be less than 5mb!") 
-    else:
-        await msg.edit_text(f'**Uploaded To Telegraph!\n\n👉 https://telegra.ph{response[0]}\n\nJoin **',
-            disable_web_page_preview=True,
-        )
-    finally:
-        os.remove(download_location)
+        
+    
