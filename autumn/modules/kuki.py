2c94ccc7fa3831dbf178e3127904d37014b8be95
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-10-01 01:27:41 +0000

    Update kuki.py

--- autumn/modules/kuki.py
+++ autumn/modules/kuki.py
@@ -22,20 +22,23 @@ buttons = [[InlineKeyboardButton("Github", url="https://github.com/famouskaykay/
     group=2,
 )
 async def kukiai(client: Client, message: Message):
-    msg = message.text
-    chat_id = message.chat.id
+  msg = message.text
+  chat_id = message.chat.id
 
-    Kuki =   requests.get(f"https://kuki-api.tk/api/botname/owner/message={msg}").json()
-
-    moezilla = f"{Kuki['reply']}"
-      
-    await client.send_chat_action(message.chat.id, "typing")
-    await message.reply_text(moezilla)
- 
-  
+  if message.text and not message.document:
+        if not kuki_message(context, message):
+            return
+        Message = message.text
+        kuki.send_chat_action(chat_id, action="typing")
+        kukiurl = requests.get('https://kuki-api.tk/api/Raiden/moezilla/message='+Message)
+        Kuki = json.loads(kukiurl.text)
+        kuki = Kuki['reply']
+        sleep(0.3)
+        message.reply_text(kuki, timeout=60)
     
 @kaykay.on_message(filters.command(["help", "start"]))
 async def hello(client, message):
+  
     await message.reply_text(f"Hello {message.from_user.mention},", reply_markup=InlineKeyboardMarkup(buttons))
 
     
