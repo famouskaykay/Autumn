22f6f0fa89c85e1346800c3125a14a1798399e93
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-10-01 00:47:40 +0000

    Update __init__.py

--- autumn/__init__.py
+++ autumn/__init__.py
@@ -9,13 +9,16 @@ from os import path
 from aiogram import Bot, Dispatcher, types
 from pyromod import listen
 
+from telegraph import Telegraph
+
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 kaykay = Client("autumn", api_id=APP_ID, api_hash=API_HASH, bot_token=TOKEN)
-
+print("[INFO]: STARTING BOT CLIENT ")
+kaykay.start()
 
 LOG_GROUP_ID = LOG_GROUP_ID
 
@@ -24,5 +27,15 @@ MOD_NOLOAD = []
 bot_start_time = time.time()
 
 
-print("[INFO]: STARTING BOT CLIENT ")
-kaykay.start()
+
+
+print("[INFO]: GATHERING PROFILE INFO")
+BOT_NAME = kevin.first_name + (kevin.last_name or "")
+BOT_USERNAME = kevin.username
+BOT_MENTION = kevin.mention
+
+kevin = kaykay.get_me()
+
+telegraph = Telegraph()
+telegraph.create_account(short_name=BOT_NAME)
+
