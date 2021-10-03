fec3464c8722774849fd83e4599ba822c739d499
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-10-03 10:02:27 +0000

    Update __main__.py

--- autumn/__main__.py
+++ autumn/__main__.py
@@ -1,55 +1,159 @@
 import importlib
-from kaysconfig import LOG_GROUP_ID
-import asyncio
-import uvloop
-from autumn import kaykay
-from pyrogram import idle, Client
 import time
+import re
+from sys import argv
+from typing import Optional
 
-from autumn.modules import *
+from autumn import (
+    ALLOW_EXCL,
+    CERT_PATH,
+    DONATION_LINK,
+    LOGGER,
+    OWNER_ID,
+    PORT,
+    TOKEN,
+    URL,
+    WEBHOOK,
+    SUPPORT_CHAT,
+    dispatcher,
+    StartTime,
+    telethn,
+    updater,
+    pbot)
+
 from autumn.modules import ALL_MODULES
+from autumn.modules.helper_funcs.chat_status import is_user_admin
+from autumn.modules.helper_funcs.misc import paginate_modules
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
+from telegram.error import (
+    BadRequest,
+    ChatMigrated,
+    NetworkError,
+    TelegramError,
+    TimedOut,
+    Unauthorized,
+)
+from telegram.ext import (
+    CallbackContext,
+    CallbackQueryHandler,
+    CommandHandler,
+    Filters,
+    MessageHandler,
+)
+from telegram.ext.dispatcher import DispatcherHandlerStop, run_async
+from telegram.utils.helpers import escape_markdown
+
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+
+    while count < 4:
+        count += 1
+        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+
+    return ping_time
 
 
-loop = asyncio.get_event_loop()
+PM_START_TEXT = """
+ I am Groups Management Bot !! I can Help You Manage Your Group for you!!, send /help for command list...[.](https://telegra.ph/file/36f504f4c0c2f0f138806.jpg)
+"""
+
+HELP_STRINGS = """
+Hey there, I'm Autumn!
+To make me functional, make sure that i have enough rights in your group.
+Helpful commands:
+- /start: Starts me! You've probably already used this.
+- /help: Sends this message; I'll tell you more about myself!
+- /donate: Gives you info on how to support me and my creator.
+Join My Support If You Have Any Queries: @NicoRobin_Support
+All commands can be used with the following: / !
+List of all the Modules:
+""".format(
+    dispatcher.bot.first_name,
+    "" if not ALLOW_EXCL else "👀 All commands can either be used with / or !.",
+)
+
+EREN_IMG = "https://telegra.ph/file/3e06334617f26c66c4f2b.jpg"
+
+DONATE_STRING = """xkaykay Is my boyfriend By The Way.."""
+
+IMPORTED = {}
+MIGRATEABLE = []
+HELPABLE = {}
+STATS = []
+USER_INFO = []
+DATA_IMPORT = []
+DATA_EXPORT = []
+CHAT_SETTINGS = {}
+USER_SETTINGS = {}
+
+for module_name in ALL_MODULES:
+    imported_module = importlib.import_module("autumn.modules." + module_name)
+    if not hasattr(imported_module, "__mod_name__"):
+        imported_module.__mod_name__ = imported_module.__name__
+
+    if imported_module.__mod_name__.lower() not in IMPORTED:
+        IMPORTED[imported_module.__mod_name__.lower()] = imported_module
+    else:
+        raise Exception("Can't have two modules with the same name! Please change one")
+
+    if hasattr(imported_module, "__help__") and imported_module.__help__:
+        HELPABLE[imported_module.__mod_name__.lower()] = imported_module
+
+    # Chats to migrate on chat_migrated events
+    if hasattr(imported_module, "__migrate__"):
+        MIGRATEABLE.append(imported_module)
+
+    if hasattr(imported_module, "__stats__"):
+        STATS.append(imported_module)
+
+    if hasattr(imported_module, "__user_info__"):
+        USER_INFO.append(imported_module)
+
+    if hasattr(imported_module, "__import_data__"):
+        DATA_IMPORT.append(imported_module)
+
+    if hasattr(imported_module, "__export_data__"):
+        DATA_EXPORT.append(imported_module)
+
+    if hasattr(imported_module, "__chat_settings__"):
+        CHAT_SETTINGS[imported_module.__mod_name__.lower()] = imported_module
+
+    if hasattr(imported_module, "__user_settings__"):
+        USER_SETTINGS[imported_module.__mod_name__.lower()] = imported_module
 
 
-async def start_bot():
-    bot_modules = ""
-    j = 1
-    for i in ALL_MODULES:
-        if j == 4:
-            bot_modules += "|{:<15}|\n".format(i)
-            j = 0
-        else:
-            bot_modules += "|{:<15}".format(i)
-        j += 1
-    print(
-        "+_________________________________________________________________"
+# do not async
+def send_help(chat_id, text, keyboard=None):
+    if not keyboard:
+        keyboard = InlineKeyboardMarkup(paginate_modules(0, HELPABLE, "help"))
+    dispatcher.bot.send_message(
+        chat_id=chat_id,
+        text=text,
+        parse_mode=ParseMode.MARKDOWN,
+        disable_web_page_preview=True,
+        reply_markup=keyboard,
     )
-    print(
-        "|                           Autumn                               |"
-    )
-    print(
-        "+_________________________________________________________________+"
-    )
-    print(bot_modules)
-    print(
-        "+_________________________________________________________________+"
-    )
-    print(f"[INFO]: BOT STARTED !")
-    try:
-        print("[INFO]: SENDING ONLINE STATUS")
-        await kaykay.send_message(LOG_GROUP_ID, "autumn has started!")
-    except Exception:
-        pass
-    await idle()
-    print("[INFO]: STOPPING BOT  ")   
-
-print("[INFO]: INITIALIZING ")
 
 
+@run_async
+def test(update: Update, context: CallbackContext):
+    # pprint(eval(str(update)))
+    # update.effective_message.reply_text("Hola tester! _I_ *have* `markdown`", parse_mode=ParseMode.MARKDOWN)
+    update.effective_message.reply_text("This person edited a message")
+    print(update.effective_message)
 
-
-if __name__ == "__main__":
-    uvloop.install()
-    loop.run_until_complete(start_bot())
