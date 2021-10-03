5c1d80c08d7b5fb41830555918917bab723542ca
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-10-03 17:29:17 +0000

    Update config.py

--- autumn/config.py
+++ autumn/config.py
@@ -1,6 +1,10 @@
 import json
 import os
 
+def get_user_list(config, key):
+    with open("{}/autumn/{}".format(os.getcwd(), config), "r") as json_file:
+        return json.load(json_file)[key]
+
 
 
 # Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
