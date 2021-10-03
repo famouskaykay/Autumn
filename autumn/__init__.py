ad972fe59a24d7d763e588fa2d47e11e43460bb8
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-10-03 14:46:17 +0000

    Update __init__.py

--- autumn/__init__.py
+++ autumn/__init__.py
@@ -195,7 +195,7 @@ DEMONS = list(DEMONS)
 TIGERS = list(TIGERS)
 
 # Load at end to ensure all prev variables have been set
-from SaitamaRobot.modules.helper_funcs.handlers import (
+from autumn.modules.helper_funcs.handlers import (
     CustomCommandHandler,
     CustomMessageHandler,
     CustomRegexHandler,
